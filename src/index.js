7c035494f428b6582b8102096937479401a6db03
Paul Tiry <tirypaul@gmail.com>
2022-11-02 13:20:47 +0100

    setup and global styles

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import GlobalStyles from './styles/GlobalStyles'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(<App />)
+root.render(
+  <>
+    <GlobalStyles />
+    <App />
+  </>
+)
---
+++ src/styles/GlobalStyles.js
@@ -0,0 +1,19 @@
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyles = createGlobalStyle`
+    * {
+        box-sizing: border-box;
+        margin: 0;
+        padding: 0;
+    }
+    body {
+        font-family: 'Montserrat', 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+    }
+    #root {
+        min-width: 100vw;
+        min-height: 100vh;
+        display: flex;
+        background-color: #1B1D29;
+    }
+`
+export default GlobalStyles
